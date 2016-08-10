4e52127a91701558fab7b0acd527da5c09b0f7c6
Stacy Zolnikov <zolnikovstacy@gmail.com>
2016-08-10 11:39:24 -0400

    Added Button

--- MyApplication/app/src/main/java/com/example/stacyzolnikov/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/stacyzolnikov/myapplication/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.stacyzolnikov.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.buttonDoestExist);
     }
 }
